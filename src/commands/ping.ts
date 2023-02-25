7b55aed00cf1a1328efbea6a87d8b513beaace6d
Ben Schlegel <ben5.schlegel@gmail.com>
2023-02-25 11:16:43 +0100

    feat(logging): update logging

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,12 +1,17 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
 import { backgroundColor } from '../utils/constants';
 import { SlashCommand } from '../types';
+import { stringify, updateUserbase } from '../functions';
 
 const command : SlashCommand = {
 	command: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Shows the bot\'s ping'),
 	async execute(interaction) {
+		const commandUserId = interaction.user.id;
+		updateUserbase(commandUserId);
+
+		console.log('level=trace command="/ping" userId=' + commandUserId + ' username="' + interaction.user.username + '"');
 		interaction.reply({
 			embeds: [
 				new EmbedBuilder()
@@ -14,6 +19,8 @@ const command : SlashCommand = {
 					.setDescription(`🏓 Pong! \n 📡 Ping: ${interaction.client.ws.ping}`)
 					.setColor(backgroundColor),
 			],
+		}).catch(err => {
+			console.log('level=error command="/makeChoice" error="' + stringify(err) + '"');
 		});
 	},
 };
