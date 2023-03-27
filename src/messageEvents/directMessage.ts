6e70be7fbffa858c59a2d1a3aac19424e06d63c2
Ben Schlegel <ben5.schlegel@gmail.com>
2023-03-27 23:20:15 +0200

    feat(dm-choice): reply with yes/no choice to dms
    also add reusable "makeYesNoChoice" embed to utils

--- src/messageEvents/directMessage.ts
+++ src/messageEvents/directMessage.ts
@@ -1,6 +1,14 @@
 import { Message } from 'discord.js';
+import { makeYesNoChoice } from 'src/utils/makeYesNoChoice';
 
 export async function handleDM(message: Message) {
-	console.log('message: ', message.content);
-	message.reply(message.content);
+	// TODO: add prom
+	// Get promt from message and create embed
+	const promtName = message.content;
+	const yesNoEmbed = makeYesNoChoice(promtName);
+
+	console.log(yesNoEmbed);
+
+	// Reply with embed, disallow mentions so reply is not highlighted
+	message.reply({ embeds: [yesNoEmbed], allowedMentions: { users: [] } });
 }
---
+++ src/utils/makeYesNoChoice.ts
@@ -0,0 +1,36 @@
+import { APIEmbedField, EmbedBuilder } from 'discord.js';
+import { OriginalPollEmbed } from '../components/embeds';
+import { getEmojiFromIndexWithChoice, randomEntriesFromArray } from '../functions';
+
+/**
+ * locally creates and STARTS yes/no choice with given title
+ * @param title title of choice
+ */
+export function makeYesNoChoice(title: string): EmbedBuilder {
+	const choices = ['yes', 'no'];
+	// Get original embed
+	const choiceEmbed = EmbedBuilder.from(OriginalPollEmbed).setAuthor(null);
+
+	// New fields
+	const newFields: APIEmbedField[] = [
+		{ name: '📊 Prompt', value: title },
+		{ name: '\u200B', value: '\u200B' },
+	];
+
+	// Make choice (get 1 random entry from array)
+	const result = randomEntriesFromArray(1, choices)[0];
+
+	// Add to embed
+	for (let i = 0; i < choices.length; i++) {
+		newFields.push({ name: getEmojiFromIndexWithChoice(i, choices[i]) + ' Choice', value: choices[i] });
+	}
+
+	// Add placeholder
+	newFields.push({ name: '\u200B', value: '\u200B' });
+
+	// Add decision (getEmoji(0), since it can only contain yes/no, which automatically gets transformed to correct emoji no matter the index)
+	newFields.push({ name: '⚡ Final Decision', value: getEmojiFromIndexWithChoice(0, result) + ' ' + result });
+
+	choiceEmbed.addFields(newFields);
+	return choiceEmbed;
+}
